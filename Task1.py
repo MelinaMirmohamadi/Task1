5ceec1a4e7bf5650dac4557c359e05ab83cb53f2
Dropino <mirmohamadiimelina>
2024-01-13 15:35:16 +0330

    Fix Task1 to the task branch

--- Task1.py
+++ Task1.py
@@ -1,17 +1,59 @@
 import requests
+from bs4 import BeautifulSoup
 import re
 
-url = "https://fa.wikipedia.org/"
-response = requests.get(url)
+def get_all_wikipedia_titles():
+    base_url = 'https://fa.wikipedia.org/w/api.php'
+    params = {
+        'action': 'query',
+        'list': 'allpages',
+        'aplimit': 'max',
+        'format': 'json'
+    }
 
-if response.status_code == 200:
-    data = response.text
+    titles = []
 
-    data = re.sub(r'<.*?>', '', data)
-    words = data.split(' ')
-    word_count = len(words)
+    while True:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+        
+        for page in data['query']['allpages']:
+            titles.append(page['title'])
 
-    print(f"تعداد کلمات در داده متنی ویکی پدیا فارسی: {word_count}")
+        if 'continue' in data:
+            params['apcontinue'] = data['continue']['apcontinue']
+        else:
+            break
 
-else:
-    print("دریافت اطلاعات با شکست مواجه شد")
+    return titles
+
+def count_words_in_wikipedia_page(page_title):
+    url = f'https://fa.wikipedia.org/wiki/{page_title}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        paragraphs = soup.find_all('p')
+
+        total_words = 0
+        for paragraph in paragraphs:
+            text = paragraph.get_text()
+            words = re.findall(r'\b\w+\b', text)
+            total_words += len(words)
+
+        return total_words
+    else:
+        return 0
+
+# گرفتن تمام عناوین صفحات
+all_titles = get_all_wikipedia_titles()
+
+total_word_count = 0  # متغیر برای جمع کل کلمات
+
+for title in all_titles:
+    word_count = count_words_in_wikipedia_page(title)
+    total_word_count += word_count
+    print(f"تعداد کلمات در صفحه '{title}': {word_count}")
+
+# چاپ جمع کل کلمات تمام صفحات
+print(f"تعداد کلمات در تمام صفحات: {total_word_count}")
